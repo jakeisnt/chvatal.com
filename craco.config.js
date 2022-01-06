9e6fb9509ee875e3061f7a4aecfa21a4cf6a8062
Jacob Chvatal <jake@isnt.online>
2022-01-06 03:29:50 -0500

    add react spring example

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,14 @@
 // craco.config.js
 const CracoEsbuildPlugin = require('craco-esbuild');
+const { ProvidePlugin } = require('webpack');
 
 module.exports = {
+  webpack: {
+    plugins: [
+      new ProvidePlugin({
+        React: 'react',
+      })
+    ]
+  },
   plugins: [{ plugin: CracoEsbuildPlugin }],
 };
